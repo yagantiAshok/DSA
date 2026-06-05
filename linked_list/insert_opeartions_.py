c34c29ee45b824edc27916f08e768b5e4d8f7694
yagantiAshok <yagantiashok177@gmail.com>
2026-06-05 16:10:34 +0530

    linked list

--- linked_list/insert_opeartions_.py
+++ linked_list/insert_opeartions_.py
@@ -27,12 +27,12 @@ def take_input():
             head = newnode
 
             next = newnode
-        
-        next.ref = newnode
+        else:
+            next.ref = newnode
 
-        next = newnode
-        
-        
+            next = newnode
+            
+            
         # else:
 
         #     temp = head
@@ -48,14 +48,27 @@ def take_input():
     return head
 
 head = take_input()
+def print_list(head):
 
-def insert_at_head(head,data):
+    
+    temp = head 
 
-    new_node = Node(data)
+    while temp!=None:
 
-    new_node.ref = head
+        print(temp.data,end="->")
 
-    return new_node
+
+        temp = temp.ref
+
+print_list(head)
+
+# def insert_at_head(head,data):
+
+#     new_node = Node(data)
+
+#     new_node.ref = head
+
+#     return new_node
 
 # new_head = insert_at_head(head,8)
 
@@ -80,75 +93,162 @@ def insert_at_head(head,data):
 # head = insert_at_tail(head,0)
 
 
-def insert_at_index(head,index,data):
+# def insert_at_index(head,index,data):
 
-    if index==0:
+#     if index==0:
 
-        return insert_at_head(head,data)
+#         return insert_at_head(head,data)
 
-    temp = head
+#     temp = head
 
+#     count = 1
+
+#     while (temp!=None and count<index-1):
+
+#         count+=1
+
+#         temp = temp.ref
+
+#     if temp is None:
+
+#         print( "Index out of range ")
+
+#         return head
+
+    
+#     newnode = Node(data)
+
+#     newnode.ref = temp.ref
+
+#     temp.ref = newnode
+
+#     return head
+
+
+# head = insert_at_index(head,3,0)
+
+
+
+# def insert_at_index_recursively(head,index,data):
+
+#     if (index == 0):
+
+#         newnode = Node(data)
+
+#         newnode.ref = head
+
+#         return newnode
+    
+#     if (head is None):
+
+#         print("Index out of range")
+
+#         return head
+    
+#     head.ref = insert_at_index_recursively(head.ref,index-1,data)
+
+#     return head 
+
+# head = insert_at_index_recursively(head,4,100)
+
+
+# deleting oprations
+
+# def delete_head_node(head):
+
+#     if (head is None):
+
+#         return "no elements"
+    
+#     new_head = head.ref
+
+#     return new_head
+    
+# head = delete_head_node(head)
+
+# print(head)
+
+# def delete_tail_node(head):
+
+#     if (head is None) :# if (head is None or head.ref==None):
+
+#         return None
+    
+#     if (head.ref==None):
+
+#         return delete_head_node(head)
+    
+#     previous_node = head
+
+#     temp = head
+
+#     while temp.ref!=None:# temp.ref.ref
+
+#         previous_node  = temp
+
+#         temp = temp.ref
+    
+#     previous_node.ref = temp.ref
+
+#     return head
+
+# head = delete_tail_node(head)
+
+# print(head)
+
+# def recursively_remove_tail(head):
+
+#     if (head is None or head.ref == None):
+
+#         return None
+    
+#     head.ref = recursively_remove_tail(head.ref)
+
+#     return head
+
+
+# head = recursively_remove_tail(head)
+
+
+def delete_at_index(head,index):
+
+    if ( index==0 ):
+
+        head = head.ref
+
+        return head 
+    
+    if (head is None):
+
+        return None
+    
     count = 0
+    temp = head
 
     while (temp!=None and count<index-1):
 
-        count+=1
-
         temp = temp.ref
 
-    if temp is None:
+        count+=1
 
-        print( "Index out of range ")
+    if (temp is None or temp.ref==None):
 
-        return head
-
+        return "index out of range"
     
-    newnode = Node(data)
+    tail_end = temp.ref
 
-    newnode.ref = temp.ref
-
-    temp.ref = newnode
+    temp.ref = tail_end.ref
 
     return head
 
 
-# head = insert_at_index(head,0,0)
+head = delete_at_index(head,3)
 
+print(head)
 
+print()
 
-def insert_at_index_recursively(head,index,data):
-
-    if (index == 0):
-
-        newnode = Node(data)
-
-        newnode.ref = head
-
-        return newnode
-    
-    if (head is None):
-
-        print("Index out of range")
-
-        return head
-    
-    head.ref = insert_at_index_recursively(head.ref,index-1,data)
-
-    return head 
-
-head = insert_at_index_recursively(head,4,100)
-
-def print_list(head):
-
-    
-    temp = head 
-
-    while temp!=None:
-
-        print(temp.data,end="->")
-
-        temp = temp.ref
-        
+print("After deleted")
 
 print_list(head)
 
