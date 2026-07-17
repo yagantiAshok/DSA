1a60611730d867dd4f2dfb61718f4d9392c849c3
yagantiAshok <yagantiashok177@gmail.com>
2026-07-17 17:31:34 +0530

    largest value in each row

---
+++ Trees/generic_tree/large_data_in_row.py
@@ -0,0 +1,39 @@
+
+
+from collections import deque
+from take_input import take_input_level_wise
+# def large_data_in_each_level(root):
+#     data = []
+#     data.append(root.data)
+#     queue = deque([root])
+#     while len(queue)!=0:
+#         root = queue.popleft()
+#         large = 0
+#         for child in root.children:
+#             if child.data>large:
+#                 large = child.data
+#             queue.append(child)
+#         if len(root.children)!=0:
+#          data.append(large)
+#     return data
+# root = take_input_level_wise()
+# print(large_data_in_each_level(root))
+
+def large_data_in_each_level(root):
+    result = []
+    if root is None:
+        return []
+    queue = deque([root])
+    while queue:
+        level_size = len(queue)
+        largest = float("-inf")
+        for _ in range(level_size):
+            node = queue.popleft()
+            if node.data>largest:
+                largest=node.data
+            for child in node.children:
+                queue.append(child)
+        result.append(largest)
+    return result
+root = take_input_level_wise()
+print(large_data_in_each_level(root))
---
+++ Trees/generic_tree/sum_nodes.py
@@ -0,0 +1,13 @@
+
+
+# from take_input import take_input_level_wise
+
+# def sum_nodes(root):
+#     if (root==None):
+#         return 0 
+#     data = root.data
+#     for child in root.children:
+#         data = data + sum_nodes(child)
+#     return data
+# root = take_input_level_wise()
+# print(sum_nodes(root))
