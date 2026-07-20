e0e7f36c442174904f399dcde59cf645c08beff2
yagantiAshok <yagantiashok177@gmail.com>
2026-07-20 18:15:37 +0530

    diameter of binary tree

---
+++ Binary_Trees/diameter.py
@@ -0,0 +1,20 @@
+
+
+from take_input import take_input_level_wise
+
+root = take_input_level_wise()
+
+answer = [0]
+def diameter_of_tree(root):
+    
+    if (root is None):
+        return 0
+    left_tree_height = diameter_of_tree(root.left)
+    right_tree_height = diameter_of_tree(root.right)
+    diameter = left_tree_height+right_tree_height
+    if diameter>answer[0]:
+        answer[0]=diameter
+    return 1 + max(left_tree_height,right_tree_height)
+
+height = diameter_of_tree(root)
+print(f"Diameter {answer[0]} and height is {height} ")
--- Binary_Trees/take_input.py
+++ Binary_Trees/take_input.py
@@ -3,38 +3,38 @@
 from implementation import BinaryTreeNode,print_binary_tree_node
 from collections import deque
 
-# def take_input_level_wise():
-#     data = int(input("Enter data for the root node : "))
-#     root = BinaryTreeNode(data)
-#     queue = deque([root])
-#     while queue:
-#         node = queue.popleft()
-#         left = int(input(f"Enter left data for {node.data} or -1 for ending: "))
-#         right = int(input(f"Enter right data for {node.data} or -1 for ending:  "))
-#         if left!=-1:
-#             node.left = BinaryTreeNode(left)
-#             queue.append(node.left)
-#         if right!=-1:
-#             node.right = BinaryTreeNode(right)
-#             queue.append(node.right)
+def take_input_level_wise():
+    data = int(input("Enter data for the root node : "))
+    root = BinaryTreeNode(data)
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        left = int(input(f"Enter left data for {node.data} or -1 for ending: "))
+        right = int(input(f"Enter right data for {node.data} or -1 for ending:  "))
+        if left!=-1:
+            node.left = BinaryTreeNode(left)
+            queue.append(node.left)
+        if right!=-1:
+            node.right = BinaryTreeNode(right)
+            queue.append(node.right)
 
-#     return root
+    return root
 
 # root = take_input_level_wise()
 # print_binary_tree_node(root)
 
 
-def take_input():
-    data = int(input("Enter data for node or -1 to end:"))
-    if data==-1:
-        return None
+# def take_input():
+#     data = int(input("Enter data for node or -1 to end:"))
+#     if data==-1:
+#         return None
     
-    node = BinaryTreeNode(data)
-    print(f"Enter left child data for {data}")
-    node.left = take_input()
-    print(f"Enter data for right child {data}")
-    node.right = take_input()
-    return node
+#     node = BinaryTreeNode(data)
+#     print(f"Enter left child data for {data}")
+#     node.left = take_input()
+#     print(f"Enter data for right child {data}")
+#     node.right = take_input()
+#     return node
 
-root = take_input()
-print_binary_tree_node(root)
+# root = take_input()
+# print_binary_tree_node(root)
