06471d90ff8e7051ba9db15dc61ade4017950440
yagantiAshok <yagantiashok177@gmail.com>
2026-08-08 18:08:44 +0530

    revisted problems

--- Binary_Trees/diameter.py
+++ Binary_Trees/diameter.py
@@ -3,10 +3,8 @@
 from take_input import take_input_level_wise
 
 root = take_input_level_wise()
-
 answer = [0]
 def diameter_of_tree(root):
-    
     if (root is None):
         return 0
     left_tree_height = diameter_of_tree(root.left)
@@ -15,7 +13,5 @@ def diameter_of_tree(root):
     if diameter>answer[0]:
         answer[0]=diameter
     return 1 + max(left_tree_height,right_tree_height)
-
 height = diameter_of_tree(root)
-
 print(f"Diameter {answer[0]} and height is {height} ")
